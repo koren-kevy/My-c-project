ee471ed4fa72e114f7b34d4555d7d08059e046c3
koren-kevy <korenlevy29@gmail.com>
2024-10-29 18:13:50 +0200

    Create screen prints for letters.c

---
+++ screen prints for letters.c
@@ -0,0 +1,23 @@
+Enter text
+I am young. You are young. All of us are young.
+"I think we need some help. Please" HELP. NO, NO NO,
+I DO NOT
+ NEED HELP
+WHATSOEVER.
+"Today’s date is
+ 15/2/2021"...
+I am 18 years old, are you 20 years old? Maybe 30 years?I am young. You are young. All of us are young.
+
+"I THINK WE NEED SOME HELP. PLEASE" Help. No, no no,
+
+i do not
+
+ need help
+
+whatsoever.
+
+"TODAY'S DATE IS
+
+ //"...
+
+I am  years old, are you  years old? maybe  years?
